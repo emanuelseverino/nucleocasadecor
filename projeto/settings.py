fe4e8fa93ebfc389a6591696ffab64a56adb08f6
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-01 14:39:25 -0300

    Corrigindo os statics

--- projeto/settings.py
+++ projeto/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,12 +115,12 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = (BASE_DIR / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'statics',
 # ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 AUTH_USER_MODEL = 'usuario.Usuario'
 
