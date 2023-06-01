3a617882e9398cb949b8a9c18ee0dd9b4baec18b
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-01 14:28:39 -0300

    segundo commit

--- projeto/settings.py
+++ projeto/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-^1((t*f+5@zez8s7gffn*%u!rm3tms^^ivnm#4(%$9^zyteh@x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 # Application definition
 
