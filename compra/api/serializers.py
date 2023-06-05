005670886fbc19ee24fb5b91b26a192fcacf618c
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-05 14:37:59 -0300

    Rodou

--- compra/api/serializers.py
+++ compra/api/serializers.py
@@ -3,22 +3,24 @@ from rest_framework.serializers import ModelSerializer
 
 from compra.models import Compra
 
+Usuario = get_user_model()
+
 
 class EmpresaSerializer(ModelSerializer):
     class Meta:
-        model = get_user_model()
+        ref_name = "Empresa"
+        model = Usuario
         fields = ['foto', 'first_name', 'last_name', 'email', 'tipo']
 
 
 class EspecificadorSerializer(ModelSerializer):
     class Meta:
-        model = get_user_model()
+        ref_name = "Especificado"
+        model = Usuario
         fields = ['foto', 'first_name', 'last_name', 'email', 'tipo']
 
 
 class CompraSerializer(ModelSerializer):
-    empresa = EmpresaSerializer()
-    especificador = EspecificadorSerializer()
 
     class Meta:
         model = Compra
