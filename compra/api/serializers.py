b2c0fac4505f3bd717a67aa89779c41d806897e6
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-05 14:24:43 -0300

    Compras Serializer

--- compra/api/serializers.py
+++ compra/api/serializers.py
@@ -1,10 +1,25 @@
+from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
 
 from compra.models import Compra
 
 
+class EmpresaSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['foto', 'first_name', 'last_name', 'email', 'tipo']
+
+
+class EspecificadorSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['foto', 'first_name', 'last_name', 'email', 'tipo']
+
 
 class CompraSerializer(ModelSerializer):
+    empresa = EmpresaSerializer()
+    especificador = EspecificadorSerializer()
+
     class Meta:
         model = Compra
         fields = "__all__"
