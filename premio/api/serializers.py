bf8dba4728050042dda3efbf6395a0bf9c880e8f
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-22 13:39:52 -0300

    Premio

--- premio/api/serializers.py
+++ premio/api/serializers.py
@@ -6,13 +6,6 @@ from premio.models import Premio
 
 class PremioSerializer(ModelSerializer):
 
-    imagem_1 = Base64ImageField(requerid=True)
-    imagem_2 = Base64ImageField(requerid=False)
-    imagem_3 = Base64ImageField(requerid=False)
-    imagem_4 = Base64ImageField(requerid=False)
-    imagem_5 = Base64ImageField(requerid=False)
-    imagem_6 = Base64ImageField(requerid=False)
-
     class Meta:
         model = Premio
         fields = "__all__"
