08664a350f3369a1c78fe0f509d605f77a5f8b1f
Emanuel Severino <emanuelsmseverino@icloud.com>
2023-06-22 13:35:45 -0300

    Premio

--- premio/api/serializers.py
+++ premio/api/serializers.py
@@ -1,9 +1,18 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework.serializers import ModelSerializer
 
 from premio.models import Premio
 
 
 class PremioSerializer(ModelSerializer):
+
+    imagem_1 = Base64ImageField(requerid=True)
+    imagem_2 = Base64ImageField(requerid=False)
+    imagem_3 = Base64ImageField(requerid=False)
+    imagem_4 = Base64ImageField(requerid=False)
+    imagem_5 = Base64ImageField(requerid=False)
+    imagem_6 = Base64ImageField(requerid=False)
+
     class Meta:
         model = Premio
         fields = "__all__"
